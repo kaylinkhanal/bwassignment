113aa838d00fc54d4c7dfc1b5e96a0bd28a8ab27
Kaylin Khanal <kyalinnop@gmail.com>
2024-05-02 07:51:39 +0545

    add tailwind.config.js

--- nextjs/src/app/page.js
+++ nextjs/src/app/page.js
@@ -1,17 +1,36 @@
-const page = () => {
-  //jsx- > javascript xml format
+import React from "react";
+import {Card,Input, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
+
+export default function App() {
   return (
-    <div>
-      <p style={{margin:'30px', backgroundColor:'pink' }}><b>LOGIN:</b></p>
-      <input placeholder="enter username"/><br/>
-      <input placeholder="enter password"/><br/>
-      <button>Click me</button>
-    </div>
-  )
+    <Card className="max-w-[400px]">
+      <CardHeader className="flex gap-3">
+        <Image
+          alt="nextui logo"
+          height={40}
+          radius="sm"
+          src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+          width={40}
+        />
+        <div className="flex flex-col">
+          <p className="text-md">LOGIN</p>
+        </div>
+      </CardHeader>
+      <Divider/>
+      <CardBody>
+      <Input type="email" label="Email" />
+      <Input type="password" label="Password" />
+      </CardBody>
+      <Divider/>
+      <CardFooter>
+        <Link
+          isExternal
+          showAnchorIcon
+          href="https://github.com/nextui-org/nextui"
+        >
+          Visit source code on GitHub.
+        </Link>
+      </CardFooter>
+    </Card>
+  );
 }
-
-export default page
-
-//1. using external css className=""
-//2. using inline css using style=
-//3. using tailwind
