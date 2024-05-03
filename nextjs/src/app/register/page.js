7972c4774eca8861f8ff4a829752fb986e7d5c79
Kaylin Khanal <kyalinnop@gmail.com>
2024-05-03 07:36:58 +0545

    login register page created

--- nextjs/src/app/register/page.js
+++ nextjs/src/app/register/page.js
@@ -5,16 +5,18 @@ import Link from "next/link";
 export default function App() {
   return (
     <>
-  <div className="text-orange-500 text-4xl -rotate-90 w-72">LOGIN</div>
+  <div className="text-orange-500 text-4xl -rotate-90 w-72 m-10">REGISTER</div>
     <div className="flex  justify-center items-center ">
   <div >
       <Input className="m-2" type="email" label="Email" />
       <Input className="m-2" type="password" label="Password" />
+      <Input className="m-2" label="Full Name" />
+      <Input className="m-2"  label="Address" />
       <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg m-2">
-      Login
+      Register
     </Button>
     <br/>
-    Don't have an account yet? <Link href="/register">Sign Up</Link>  instead!!
+   Already have an account? <Link href="/">Login</Link>  instead!!
   </div>
   
     </div>
