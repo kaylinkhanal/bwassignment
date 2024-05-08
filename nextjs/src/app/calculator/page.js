212b83e98f35eb077030315334e6d2a7deb55317
Kaylin Khanal <kyalinnop@gmail.com>
2024-05-08 08:04:40 +0545

    added calculator code

--- nextjs/src/app/calculator/page.js
+++ nextjs/src/app/calculator/page.js
@@ -1,7 +1,8 @@
 'use client'
-import React from 'react'
+import React,{useState} from 'react'
 
 const page = () => {
+    const [output, setOutput] = useState('')
     const calculatorKeys = [
         ['C','<-','%', '/'],
         ['7','8','9', '*'],
@@ -9,15 +10,54 @@ const page = () => {
         ['1','2','3', '+'],
         ['+/-','0','.', '='],
     ]
+    const colorConfig = {
+      orangeKeys:  ['C', '=' ],
+      crimsonKeys: ['<-','%','/','*','-','+']
+    }
+
+    const symbols = ['/','*','-','+','<-','%','C', '=','+/-' ]
+
+    const handleClick = (keys)=> {
+        debugger;
+        const removeLastChar = output.slice(0,output.length-1)
+      if(keys === 'C'){
+        setOutput('')
+      }else if(keys === '='){
+        const result = eval(output)
+        setOutput(result)
+      }else if(keys==='<-'){
+          setOutput(removeLastChar)
+      }
+      else{
+        const result = output[output.length -1]
+        if(symbols.includes(result) && Number.isNaN(Number(keys))){
+          const newVal = removeLastChar+ keys
+          setOutput(newVal)
+          return;
+        }
+        setOutput(output +keys)
+        }
+      }
+
+
   return (
-    <div>
+    <div className='bg-black'>
+      <div className='text-white text-3xl'>
+        {output}
+      </div>
    {
     calculatorKeys.map((item)=>{
            return (
             <div className='flex'>
                 { 
                 item.map((keys)=>{
-                    return <div className='p-2 m-2 w-8 shadow-lg'>{keys}</div>
+                    return <div 
+                      onClick={()=>handleClick(keys)}
+                      style={{
+                        backgroundColor: colorConfig.orangeKeys.includes(keys) ? 'orange': 'white',
+                        color: colorConfig.crimsonKeys.includes(keys) ? 'orange': 'black',
+                    }}
+                     className='p-2 m-2 w-8 shadow-lg bg-white rounded-lg'>{keys}</div>
                      })
                 }
             </div>
